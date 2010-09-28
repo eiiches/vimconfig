4cb98dd3de2ecbc7383e6c7a2c47a1e0561177c2
Eiichi Sato <sato.eiichi@gmail.com>
2010-09-28 16:39:50 +0900

    added initialize script to fetch submodules and do some make

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VIM_ROOT=$(readlink -f $(dirname $0))
+
+cd $VIM_ROOT
+git submodule init
+git submodule update
+
+(
+	cd runtime/vim-metarw
+	git submodule init
+	git submodule update
+	make
+)
+
+(
+	cd runtime/metarw-git
+	git submodule init
+	git submodule update
+	make
+)
