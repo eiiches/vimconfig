0647ad549e4df80923106a665336e15b197cfbda
Eiichi Sato <sato.eiichi@gmail.com>
2011-02-15 03:27:51 +0900

    Updated plugins

--- update.sh
+++ update.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-VIM_ROOT=$(readlink -f $(dirname $0))
-
+VIM_ROOT=$(dirname $(readlink -f $0))
 cd $VIM_ROOT
+
 git submodule init
 git submodule update
 
-(
-	cd runtime/neocomplcache && \
-	git checkout master && \
+git ls-files --stage --error-unmatch | grep '^160000' | while read mode sha1 num p
+do
+	cd $p
+	echo "$p"
+	git checkout master
 	git pull
-)
-
-(
-	cd runtime/vim-quickrun && \
-	git checkout master && \
-	git pull
-)
+	cd -
+done
 
 (
 # TODO...
-	git svn fetch vim-latex
+	#git svn fetch vim-latex
 )
 
