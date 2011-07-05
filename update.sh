ba3d8dbb0c31af53474dbf122e50390500ca39da
Eiichi Sato <sato.eiichi@gmail.com>
2011-07-05 10:56:33 +0900

    Rewrite update.sh using git submodule foreach

--- update.sh
+++ update.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-VIM_ROOT=$(dirname $(readlink -f $0))
-cd $VIM_ROOT
+scriptdir=$(dirname $(readlink -f $0))
+cd $scriptdir
 
 git submodule init
 git submodule update
-
-git ls-files --stage --error-unmatch | grep '^160000' | while read mode sha1 num p
-do
-	cd $p
-	echo "$p"
-	git checkout master
-	git pull
-	cd -
-done
+git submodule foreach 'git checkout master; git pull'
 
 ( cd runtime/vimproc && make -f make_gcc.mak )
 ( cd runtime/vim-metarw && make )
