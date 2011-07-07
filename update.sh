c18db9b99c01cb89c400f392847904adb2533141
Eiichi Sato <sato.eiichi@gmail.com>
2011-07-08 05:06:59 +0900

    Updated update.sh: Submodules must be checked out recursively.

--- update.sh
+++ update.sh
@@ -3,8 +3,7 @@
 scriptdir=$(dirname $(readlink -f $0))
 cd $scriptdir
 
-git submodule init
-git submodule update
+git submodule update --recursive --init
 git submodule foreach 'git checkout master; git pull'
 
 ( cd runtime/vimproc && make -f make_gcc.mak )
